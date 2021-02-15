99e186f6c6723ac74ed8450fb3d6698ac0bd87cc
Michael Pingel <mdpingel@gmail.com>
2021-02-15 11:53:13 -0800

    pass User() test
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- src/js/user.js
+++ src/js/user.js
@@ -1,5 +1,10 @@
 export default class User {
   constructor(name, age, gender, experience, weight, focus) {
-
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.experience = experience;
+    this.weight = weight;
+    this.focus = focus;
   }
 }
