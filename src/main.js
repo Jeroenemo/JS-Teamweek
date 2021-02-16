3bcae66204c3bd5b27042e37298634615c4b68f3
Michael Pingel <mdpingel@gmail.com>
2021-02-16 11:05:18 -0800

    remove old form and add functionality to retrieve workouts based on muscle group
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExerciseService from './services/exercise-service.js';
+
+function getExercises(response) {
+  response.results.forEach(function(exerciseObject) {
+    $('#exercise-list').append(`<p>${exerciseObject.name}</p>`);
+  });
+}
+
+$('form#category-select').submit(function(event) {
+  event.preventDefault();
+  $('#exercise-list').html("");
+  const categoryUrl = `&category=${$('#category').val()}`;
+  (async function() {
+    const response = await ExerciseService.getExercise(categoryUrl);
+    getExercises(response);
+    console.log(response);
+  })();
+});
