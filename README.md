c4ba84983d40ac97de720eb443035eb44b414b87
James Wyn <thisisjameswyn@gmail.com>
2021-02-17 17:02:41 -0800

    tie in paper doll model and add .css styling + some logic
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>
    Co-authored-by: Jeroen van Seeters <vanseetersjeroen@gmail.com>
    Co-authored-by: Mikey Kriegel <mikkrieg@gmail.com>

--- README.md
+++ README.md
@@ -1,31 +1,47 @@
-# (Application Name)
+## Workout buddy
+#### *Epicodus Week 7 Team Project  2/8-12/2021*
+***By Jeroen van Seeters, Mike Pingel, Mikey Kriegel, James Wyn, Daniel Adeyemi***
+___
 
-#### (Brief Description of Application)
+## *Description*:    
+***A web app that allows user to search for exercises based on muscle groups or keywords***
+### *???*
 
-#### By (Your Name Here)
 
-## Technologies Used
+## *Setup instructions:*
+#### From the web:
+* Go to my GitHub repository, using following [URL]().
+* Click the "Code" ![img](src/img/code.png) and click the 'Download zip' option ![img](src/img/zip.png).
+* Unzip the file, navigate to the `src` directory.
+* open ***'index.html' or any interesting for you file*** to see code.
+#### From the terminal: 
+* Clone my repository from GitHub using `git clone #`
+* Navigate to the downloaded folder using ***cd*** command
+* Create `.env` file in the top level of directory
+* Execute **code .** command in your terminal and it will open all source code in your code editor.    
+*Note: to run this project locally you will need to have Node. After you cloned and open repository - execute `npm install` in command line to gain access to webpack*
+#### API keys requirement:
+This program uses an API key from [Workout manager](https://wger.de/en/software/api).   
+You don't need special key to use this application.   
 
-* _List all_
-* _the major technologies_
-* _you used in your project_
-* _here_
+#### GitHub pages:
+*GitHub page is unavailable for this project*
 
-## Description
+## *Tecnologies used:*
+* HTML
+* CSS
+* Java Script
+* JQuery
+* Bootstrap *v5.0.0 beta*
+* Git and GitHub
+* Webpack
+* npm
 
-## Setup/Installation Requirements
+## *Known bugs:*
+???
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
+## *License and copyright:*
 
-## Known Bugs
-
-* _Any known issues_
-* _should go here_
-
-## License
-
-## Contact Information
+> ***© Jeroen van Seeters, Mike Pingel, Mikey Kriegel, James Wyn, Daniel Adeyemi, 2021***  
+> *API key from [Workout manager](https://wger.de/en/software/api)*   
+> *Licensed under [MIT license](https://mit-license.org/)*
--- src/main.js
+++ src/main.js
@@ -8,20 +8,20 @@ import ExerciseService from './services/exercise-service.js';
 
 function addImages(imagesArray) {
   imagesArray.forEach(function(imageObject) {
-    $('.exercise-description').append(`<img src="${imageObject.image}">`);
+    $('.exercise-description').append(`<img class="workout-image" src="${imageObject.image}">`);
   });
 }
 
 
-function getExercises(response){
-  for(let i = 0; i < response.results.length; i++){
+function getExercises(response) {
+  for (let i = 0; i < response.results.length; i++) {
     if (response.results[i].description.length > 20) {
       $('#exercise-list-contents').append(`<p id='exercise-${response.results[i].id}'>${response.results[i].name}</p>`);
-      $("#exercise-list").on("click",`#exercise-${response.results[i].id}`, function() {
+      $("#exercise-list").on("click", `#exercise-${response.results[i].id}`, function() {
         $('#exercise-list').slideToggle();
-        $('.exercise-description').html(`<h2>${response.results[i].name}</h2><p>${response.results[i].description}</p>`);
+        $('.desc').html(`<h2>${response.results[i].name}</h2><p>${response.results[i].description}</p>`);
         addImages(response.results[i].images);
-        $('.exercise-description').slideToggle();
+        $('.desc').slideToggle();
       });
     }
   }
@@ -35,11 +35,11 @@ $('form#category-select').submit(function(event) {
     const response = await ExerciseService.getExercise(categoryUrl);
     getExercises(response);
   })();
-  
+
 });
 
-
-$('button#start').on('click', function (event) {
+//HIIT LOGIC
+$('button#start').on('click', function(event) {
   event.preventDefault();
   const eTimer = () => {
     const timerElement = document.getElementById("e-timer");
@@ -53,12 +53,12 @@ $('button#start').on('click', function (event) {
         clearInterval(interval);
       }
       timerCounter = timerCounter - 1;
-          
+
       timerElement.innerText = timerCounter + "s";
       progressBar.value = timerCounter;
     }, 1000);
   };
-  
+
   const rTimer = () => {
     const timerElement = document.getElementById("r-timer");
     const progressBar = document.getElementById("r-progressBar");
@@ -69,14 +69,14 @@ $('button#start').on('click', function (event) {
         $('#rest-div, #exercise-div').hide();
         $('#complete-div').show();
       } else if (timerCounter === 0) {
-        sets --; 
+        sets--;
         $('#exercise-div').toggle();
         $('#rest-div').toggle();
         eTimer();
         clearInterval(interval);
       }
       timerCounter = timerCounter - 1;
-      
+
       timerElement.innerText = timerCounter + "s";
       progressBar.value = timerCounter;
     }, 1000);
@@ -85,16 +85,53 @@ $('button#start').on('click', function (event) {
   const rest = $('#rest').val();
   const totalSets = $('#sets').val();
   let sets = parseInt($('#sets').val());
-  
-  
+
+
   $('#e-timer').html(exercise);
   $('#r-timer').html(rest);
-  document.getElementById("e-progressBar").max = exercise ;
+  document.getElementById("e-progressBar").max = exercise;
   document.getElementById("e-progressBar").value = exercise;
-  document.getElementById("r-progressBar").max = rest ;
+  document.getElementById("r-progressBar").max = rest;
   document.getElementById("r-progressBar").value = rest;
   $('#exercise-div').toggle();
   $('#setup').toggle();
   eTimer();
 });
 
+//sexyDoll logic
+$(document).ready(function() {
+  $(".maleDiv").hide();
+  $(".femaleDiv").hide();
+  $('.bodyImg').click(function() {
+    const gender = $('input:radio[name=gender]:checked').val();
+    if (gender === "male") {
+      $(".maleDiv").show();
+      $(".femaleDiv").hide();
+    } else {
+      $(".femaleDiv").show();
+      $(".maleDiv").hide();
+    }
+  });
+
+  function showExercise(response) {
+    if (response.count) {
+      for (let i = 0; i < response.results.length; i++) {
+        $('.output').append(`<li class="description" id="${response.results[i].id}">${response.results[i].name}</li>`);
+        $('.output').on('click', `#${response.results[i].id}`, function() {
+          $('.desc').empty();
+          $('.desc').html(`${response.results[i].description}`);
+        });
+      }
+    }
+  }
+
+
+  $('.show').click(function() {
+    const target = `&muscles=${$(this).attr('id')}`;
+    ExerciseService.getExercise(target)
+      .then(function(response) {
+        $('.output, .desc').empty();
+        showExercise(response);
+      });
+  });
+});
